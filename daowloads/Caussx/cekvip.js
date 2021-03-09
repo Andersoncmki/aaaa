1e34e2db64797e7f71717bca84dd6dfb2a1a068a
Andersoncmki <79866064+Andersoncmki@users.noreply.github.com>
2021-03-09 15:04:15 -0400

    Add files via upload

---
+++ daowloads/Caussx/cekvip.js
@@ -0,0 +1,15 @@
+const cekvip = () => { 
+	return `           
+──────────────────
+*Nome do bot* :  NARUTIN
+──────────────────
+        『 *VIP STATUS* 』
+──────────────────
+• *Status*    : *ATIVO*
+────────────────── 
+*Status Bot:* *Online*
+──────────────────
+
+*VOCE E UM MEMBRO PREMIUM* 🐊🚩`
+}
+exports.cekvip = cekvip
